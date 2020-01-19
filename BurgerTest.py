e2b1285e49727e98762f2afa69bcc5424c48ec86
Christophe Maldivi <christophe.maldivi@orange.com>
2020-01-19 18:53:57 +0100

    ready to start

---
+++ BurgerTest.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class BurgerTest(unittest.TestCase):
+    def test_burger_can_say_hello(self):
+        # given
+        # when
+        # then
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
